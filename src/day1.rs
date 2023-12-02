e1bb5ce096972154b6f006d3ad89d655b3b265b2
Jeremy Haak <jamincan@gmail.com>
2023-12-02 11:33:02 -0500

    Minor optimizations to day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,19 +1,15 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
 
 pub const INPUT: &str = include_str!("input/day1.txt");
 
 solution!(INPUT, pt1, pt2);
 
 fn pt1_parse(value: &str) -> Result<u32> {
-    let first = value
-        .chars()
+    let mut chars = value.chars();
+    let first = chars
         .find_map(|c| c.to_digit(10))
         .with_context(|| format!("no first digit found in '{value}'"))?;
-    let last = value
-        .chars()
-        .rev()
-        .find_map(|c| c.to_digit(10))
-        .expect("first digit found, so last should succeed");
+    let last = chars.rev().find_map(|c| c.to_digit(10)).unwrap_or(first);
     Ok(first * 10 + last)
 }
 
@@ -24,52 +20,27 @@ fn pt1(input: &str) -> Result<u32> {
         .try_fold(0, |sum, c| c.map(|c| sum + c))
 }
 
-fn parse_digit(input: &str, reverse: bool) -> Result<u32> {
-    let test_fn = if !reverse {
-        str::starts_with
-    } else {
-        str::ends_with
-    };
-    if test_fn(input, "one") || test_fn(input, "1") {
-        Ok(1)
-    } else if test_fn(input, "two") || test_fn(input, "2") {
-        Ok(2)
-    } else if test_fn(input, "three") || test_fn(input, "3") {
-        Ok(3)
-    } else if test_fn(input, "four") || test_fn(input, "4") {
-        Ok(4)
-    } else if test_fn(input, "five") || test_fn(input, "5") {
-        Ok(5)
-    } else if test_fn(input, "six") || test_fn(input, "6") {
-        Ok(6)
-    } else if test_fn(input, "seven") || test_fn(input, "7") {
-        Ok(7)
-    } else if test_fn(input, "eight") || test_fn(input, "8") {
-        Ok(8)
-    } else if test_fn(input, "nine") || test_fn(input, "9") {
-        Ok(9)
-    } else {
-        let dir = if reverse { "end " } else { "start" };
-        Err(anyhow!("'{input}' does not {dir} with a digit"))
-    }
-}
-
 fn pt2_parse(input: &str) -> Result<u32> {
-    let mut start = 0;
-    let mut end = input.len();
-    let mut first: Option<u32> = None;
-    while end > 0 || start < input.len() {
-        if let Some(f) = first {
-            if let Ok(l) = parse_digit(&input[0..end], true) {
-                return Ok(10 * f + l);
-            }
-            end -= 1;
-        } else {
-            first = parse_digit(&input[start..], false).ok();
-            start += 1;
-        }
-    }
-    return Err(anyhow!("no digits found in input '{input}'"));
+    use aho_corasick::AhoCorasick;
+    use once_cell::sync::Lazy;
+
+    static DIGITS: Lazy<AhoCorasick> = Lazy::new(|| {
+        AhoCorasick::new(&[
+            r"1", r"2", r"3", r"4", r"5", r"6", r"7", r"8", r"9", r"one", r"two", r"three",
+            r"four", r"five", r"six", r"seven", r"eight", r"nine",
+        ])
+        .unwrap()
+    });
+    let mut digits = DIGITS.find_overlapping_iter(input);
+    let first = digits
+        .next()
+        .map(|m| m.pattern().as_u32() % 9 + 1)
+        .with_context(|| format!("no digit found in '{input}'"))?;
+    let last = digits
+        .last()
+        .map(|m| m.pattern().as_u32() % 9 + 1)
+        .unwrap_or(first);
+    Ok((10 * first + last) as u32)
 }
 
 fn pt2(input: &str) -> Result<u32> {
@@ -97,6 +68,15 @@ mod test {
     #[test]
     fn pt1() {
         assert_eq!(super::pt1(INPUT1).unwrap(), 142);
+    }
+
+    #[test]
+    fn pt2_parse() {
+        let results: Vec<_> = INPUT2
+            .lines()
+            .filter_map(|l| super::pt2_parse(l).ok())
+            .collect();
+        assert_eq!(results, vec![29, 83, 13, 24, 42, 14, 76]);
     }
 
     #[test]
