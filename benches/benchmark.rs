c6d397fda966c9d9869765b16aec861a6312376e
Jeremy Haak <jamincan@gmail.com>
2023-12-02 12:51:00 -0500

    Add benchmarks

---
+++ benches/benchmark.rs
@@ -0,0 +1,12 @@
+use aoc2023::SOLUTIONS;
+use criterion::{Criterion, criterion_group, criterion_main};
+
+pub fn benchmark(c: &mut Criterion) {
+    for (day, [pt1, pt2]) in SOLUTIONS.iter().enumerate() {
+        let day = day + 1;
+        c.bench_function(&format!("d{day}p1"), |b| b.iter(|| pt1()));
+        c.bench_function(&format!("d{day}p2"), |b| b.iter(|| pt2()));
+    }}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
